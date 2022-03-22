743dfd36d46ac63b42ba359758463bec423b541b
eeliacik <erhaneliacik@gmail.com>
2022-03-22 17:40:08 +0300

    login validations added

--- src/pages/authentication/LogIn.vue
+++ src/pages/authentication/LogIn.vue
@@ -2,15 +2,31 @@
   <section>
     <base-card><h2>Log In</h2></base-card>
     <base-card class="container">
-      <div class="form-block">
-        <form @submit.prevent="submitForm">
-          <div class="form-input">
+      <form @submit.prevent="submitForm">
+        <div class="form-block">
+          <div class="form-input" :class="{ invalid: !this.email.isValid }">
             <label for="email">E-mail</label>
-            <input type="email" id="email" v-model.trim="email.val" />
+            <input
+              type="email"
+              id="email"
+              v-model.trim="email.val"
+              @blur="clearValidationError('email')"
+            />
+            <p v-show="!this.email.isValid">
+              * Please enter your email address
+            </p>
           </div>
-          <div class="form-input">
+          <div class="form-input" :class="{ invalid: !this.password.isValid }">
             <label for="password">Password</label>
-            <input type="password" id="password" v-model="password.val" />
+            <input
+              type="password"
+              id="password"
+              v-model="password.val"
+              @blur="clearValidationError('password')"
+            />
+            <p v-show="!this.password.isValid">
+              * Please enter a password (min 6 characters)
+            </p>
           </div>
           <div class="form-action">
             <div class="form-action">
@@ -21,9 +37,9 @@
               </p>
             </div>
           </div>
-          <p v-if="isLoading">Signing In...</p>
-        </form>
-      </div>
+          <p v-show="isLoading">Signing In...</p>
+        </div>
+      </form>
     </base-card>
   </section>
 </template>
@@ -45,6 +61,12 @@ export default {
   },
   methods: {
     login() {
+      this.formIsValid = true;
+      this.emailValidation();
+      this.passwordValidation();
+      if (!this.formIsValid) {
+        return;
+      }
       this.isLoading = true;
       const authData = { email: this.email.val, password: this.password.val };
       this.$store
@@ -56,6 +78,22 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+    },
+    emailValidation() {
+      if (this.email.val === '' || !this.email.val.includes('@')) {
+        this.email.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    passwordValidation() {
+      if (this.password.val.length < 6) {
+        this.password.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    clearValidationError(name) {
+      this[name].isValid = true;
+      this.formIsValid = true;
     },
   },
 };
