18ac4a4031e435ae40f230be1b3e9673af447322
eeliacik <erhaneliacik@gmail.com>
2022-06-25 11:22:17 +0300

    not found page revised

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
-        <base-card>
-        <h2>Page Not Found!</h2>
-        <router-link to="/"><p>Homepage</p></router-link>
-        </base-card>
-    </div>
+  <div class="not-found-wrapper">
+    <div>Page Not Found!</div>
+    <router-link class="back-link" to="/">Back to Homepage</router-link>
+  </div>
 </template>
+
+<style lang="scss">
+.not-found-wrapper {
+  padding: 0 0.6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
